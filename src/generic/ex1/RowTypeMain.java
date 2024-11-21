d4a08675f237687774d04fb17606670eb26e73f9
SukJin Kim <rkathrwks@gmail.com>
2024-11-21 23:58:32 +0900

    feat(ex1): Generic 관련 예제 코드 작성

---
+++ src/generic/ex1/RowTypeMain.java
@@ -0,0 +1,11 @@
+package generic.ex1;
+
+public class RowTypeMain {
+
+    public static void main(String[] args) {
+        GenericBox integerBox = new GenericBox();
+        integerBox.set(10);
+        Integer integer = (Integer) integerBox.get();
+        System.out.println("integer = " + integer);
+    }
+}
