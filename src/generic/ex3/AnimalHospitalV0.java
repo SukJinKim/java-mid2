761b041e9f92501d437b5d7bbc012fa4ba047047
SukJin Kim <rkathrwks@gmail.com>
2024-11-22 00:30:23 +0900

    feat(ex3): 타입 매개변수 제한 전 예제코드 추가

---
+++ src/generic/ex3/AnimalHospitalV0.java
@@ -0,0 +1,29 @@
+package generic.ex3;
+
+import generic.animal.Cat;
+import generic.animal.Dog;
+
+public class AnimalHospitalV0 {
+
+    public static void main(String[] args) {
+        DogHospital dogHospital = new DogHospital();
+        CatHospital catHospital = new CatHospital();
+
+        Dog dog = new Dog("강아지1", 100);
+        Cat cat = new Cat("고양이1", 300);
+
+        dogHospital.set(dog);
+        dogHospital.checkup();
+
+        catHospital.set(cat);
+        catHospital.checkup();
+
+        // 문제 1 : 개 병원에 고양이 전달
+//        dogHospital.set(cat); // 다른 타입 입력시 컴파일 오류 발생
+
+        // 문제 2 : 개 타입 반환
+        dogHospital.set(dog);
+        Dog biggerDog = dogHospital.bigger(new Dog("강아지2", 200));
+        System.out.println("biggerDog = " + biggerDog);
+    }
+}
